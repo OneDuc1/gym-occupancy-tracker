d1d1fd0ceb022156610ec5fe62123ee058467b31
OneDuc1 <barkerthomas19@gmail.com>
2024-05-27 10:43:49 +0100

    Working 2024 gym.py
    Added user agent which was required to avoid getting a 403 error from the GET request, 
    Also updated URL to the one the Uni currently uses (the old one did still direct to this one though so it might not have needed to be changed). 
    Did some other formating

--- gym.py
+++ gym.py
@@ -6,41 +6,46 @@ import datetime
 import time
 import json
 import plotly
-import plotly.graph_objs as go 
+import plotly.graph_objs as go
 import pandas as pd
 
-url = "https://www.st-andrews.ac.uk/sport/"
+url = "https://sport.wp.st-andrews.ac.uk/"
 filename = "./data.json"
+
 
 def main():
     initialise(filename)
-    count=0
+    count = 0
     while True:
-        hour = int(str(datetime.datetime.now()).split(" ")[1][:6].replace(":",""))
-        if hour < 2230 and hour > 630: 
+        hour = int(str(datetime.datetime.now()).split(" ")[1][:6].replace(":", ""))
+        if 2230 > hour > 630:
             night = False
         else:
             night = True
-        try:    
+        try:
             read_web_and_add(night)
         except:
             print("reading from web failed. website might be down")
-        if(count%12==0):
+        if count % 12 == 0:
             update_plot(night)
-            count=0
+            count = 0
         time.sleep(300)
-        count+=1
+        count += 1
+
 
 def read_web_and_add(night):
     if night:
         return
     hr_timestamp = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    r = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/125.0.0.0 Safari/537.36'} 
+    r = requests.get(url, headers=headers)
     for line in r.text.split("\n"):
         if "Occupancy" in line:
-            percentage = int(line.split("Occupancy: ")[1].split("<")[0][:-1])        
-    entry = {'hr_t': hr_timestamp, 'p' : percentage}
+            percentage = int(line.split("Occupancy: ")[1].split("<")[0][:-1])
+    entry = {'hr_t': hr_timestamp, 'p': percentage}
     add_new_data(filename, entry)
+
 
 def initialise(filename):
     if os.path.isfile(filename):
@@ -48,57 +53,60 @@ def initialise(filename):
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write("")
 
+
 def add_new_data(filename, entry):
     with open(filename, mode='a+', encoding='utf-8') as f:
-        f.writelines(json.dumps(entry, sort_keys=True)+"\n")
+        f.writelines(json.dumps(entry, sort_keys=True) + "\n")
+
 
 def update_plot(night):
     if night:
         return
     df = pd.read_json(filename, lines=True)
     trace = go.Scatter(x=list(df.hr_t),
-                   y=list(df.p))
+                       y=list(df.p))
     data = [trace]
     layout = dict(
-                title='StA Gym occupancy (updated every hour)',
-                xaxis=dict(
-                    rangeselector=dict(
-                        buttons=list([
-                        dict(count=4,
-                                label='4h',
-                                step='hour',
-                                stepmode='backward'),
-                            dict(count=1,
-                                label='1d',
-                                step='day',
-                                stepmode='backward'),
-                            dict(count=7,
-                                label='1w',
-                                step='day',
-                                stepmode='backward'),
-                            dict(count=1,
-                                label='1m',
-                                step='month',
-                                stepmode='backward'),
-                            dict(count=6,
-                                label='6m',
-                                step='month',
-                                stepmode='backward'),
-                            dict(count=1,
-                                label='1y',
-                                step='year',
-                                stepmode='backward'),
-                            dict(step='all')
-                        ])
-                    ),
-                    rangeslider=dict(
-                        visible = True
-                    ),
-                    type='date'
-                )
-            )
+        title='StA Gym occupancy (updated every hour)',
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=4,
+                         label='4h',
+                         step='hour',
+                         stepmode='backward'),
+                    dict(count=1,
+                         label='1d',
+                         step='day',
+                         stepmode='backward'),
+                    dict(count=7,
+                         label='1w',
+                         step='day',
+                         stepmode='backward'),
+                    dict(count=1,
+                         label='1m',
+                         step='month',
+                         stepmode='backward'),
+                    dict(count=6,
+                         label='6m',
+                         step='month',
+                         stepmode='backward'),
+                    dict(count=1,
+                         label='1y',
+                         step='year',
+                         stepmode='backward'),
+                    dict(step='all')
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type='date'
+        )
+    )
     fig = dict(data=data, layout=layout)
     plotly.offline.plot(fig, filename='index.html', auto_open=False)
+
 
 if __name__ == "__main__":
     main()
